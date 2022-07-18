5e7fb434b796eca8dde44c52b56426005deab60c
Saloni <saloni.patil2220@gmail.com>
2022-07-18 15:34:39 +0530

    ExpenseTracker

--- expenses.js
+++ expenses.js
@@ -12,28 +12,52 @@ function addExpense(e){
 
 
     }
-    localStorage.setItem(exp.amt,JSON.stringify(exp))
+    // localStorage.setItem(exp.amt,JSON.stringify(exp))
     
-    printExpenses(exp);
+    axios.post("https://crudcrud.com/api/45174c8b0bd4455f9f8f1b663c8c7248/ExpenseTracker1",exp).then((response)=>{
+        printExpenses(response.data);
+        console.log(response)
+    }).catch((err)=>{
+        console.log(err);
+    })
+    //printExpenses(exp);
 }
     
 function printExpenses(exp){
     let formNODE = document.getElementById("formList");
     let p=exp.amt;
-    let childNODE=`<li id=${exp.amt}>${exp.amt}-${exp.cat}-${exp.man}<button onclick=deleteUser('${exp.amt}')>Delete expenses</button><button onclick=editUser('${exp.amt}','${exp.cat}','${exp.man}')>Edit expenses</button></li>`
+    let childNODE=`<li id=${exp._id}>${exp.amt}-${exp.cat}-${exp.man}<button onclick=deleteUser('${exp._id}')>Delete expenses</button><button onclick=editUser('${exp._id}','${exp.amt}','${exp.cat}','${exp.man}')>Edit expenses</button></li>`
 formNODE.innerHTML=formNODE.innerHTML+childNODE;
 
 }  
+window.addEventListener("DOMContentLoaded",()=>
+{
+   axios.get("https://crudcrud.com/api/45174c8b0bd4455f9f8f1b663c8c7248/ExpenseTracker1").then((response)=>{
+       console.log(response)
+       for(var i=0;i<response.data.length;i++){
+           printExpenses(response.data[i])
+       }
+   }).catch((err)=>{
+       document.body.innerHTML="<h4>Something went wrong<h4>"
+       console.log(err)})
+})
+function deleteUser(Id){
+    //    console.log(emailId)
+    //     localStorage.removeItem(emailId);
 
-
-    function deleteUser(exp){
-        
+        axios.delete(`https://crudcrud.com/api/45174c8b0bd4455f9f8f1b663c8c7248/ExpenseTracker1/${Id}`)
+        .then((response)=>removeUserFromScreen(Id)).catch((err)=>console.log(err))
+         //removeUserFromScreen(Id);
+}
+    function removeUserFromScreen(expID){
+       
         //console.log(p);
-        localStorage.removeItem(exp);
-        let parentNode=document.getElementById("formList");
-        let childNODE=document.getElementById(exp);
+        //localStorage.removeItem(expID);
+        const parentNode=document.getElementById("formList");
+        const childNODE=document.getElementById(expID);
         parentNode.removeChild(childNODE);
-        localStorage.removeItem(exp);
+        //localStorage.removeItem(expID);
+        //deleteUser(expID)
 
     }
 
@@ -41,12 +65,12 @@ formNODE.innerHTML=formNODE.innerHTML+childNODE;
 
 
 
-function editUser(amt1,cat1,man1){
-
-    document.getElementById("fname").value=amt1;
-    document.getElementById("desc").value=cat1;
-    document.getElementById("categ").value=man1;
-    deleteUser(amt1)
+function editUser(userId,amt1,cat1,man1){
+   
+    document.getElementById('fname').value=amt1;
+    document.getElementById('desc').value=cat1;
+    document.getElementById('categ').value=man1;
+    deleteUser(userId);
 
    
 }
